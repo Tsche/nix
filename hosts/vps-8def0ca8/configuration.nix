f5f67de98dc54ee20039651c97c58121e7fcb2ee
Matthias Wippich <mfwippich@gmail.com>
2026-05-09 03:30:15 +0200

    add wireguard

--- hosts/vps-8def0ca8/configuration.nix
+++ hosts/vps-8def0ca8/configuration.nix
@@ -14,5 +14,34 @@
     };
   };
   
+  networking.wireguard = {
+    enable = true;
+    interfaces.wg0 = {
+      ips = ["10.1.1.1/24"];
+      listenPort = 51820;
+      privateKeyFile = "/etc/wireguard/server.key";
+      postSetup = [
+        "${pkgs.iproute2}/bin/ip addr add 10.1.1.254/32 dev wg0"
+      ];
+      postShutdown = [
+        "${pkgs.iproute2}/bin/ip addr del 10.1.1.254/32 dev wg0"
+      ];
+      peers = [
+         {
+           allowedIPs = [ "10.1.1.10/32" ];
+           publicKey = "r4F6ZNrZsUKmpR+/mtWioWSCY4tYe35ttJQt9rXMI2o=";
+         }
+         {
+           allowedIPs = [ "10.1.1.20/32" ];
+           publicKey = "Bd/MO9/3jCmx2LGHC6YQxLtw16h8vRWkfNxsKtI6cS0=";
+         }
+         {
+           allowedIPs = [ "10.1.1.30/32" ];
+           publicKey = "ewc2Mco0p0mvHw88NlBeDOMQXKbyD1UXF4Wh0b+HizA=";
+         }
+      ];
+    };
+  };
+
   system.stateVersion = "25.11";
 }
