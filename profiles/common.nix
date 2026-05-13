8f54398fa3fe1118d57d66eb42cc8a0fd837de57
Matthias Wippich <mfwippich@gmail.com>
2026-05-14 00:27:04 +0200

    fix git config

--- profiles/common.nix
+++ profiles/common.nix
@@ -27,7 +27,7 @@
   programs.zsh.enable = true;
   programs.git = {
     enable = true;
-    settings = {
+    config = {
       user = {
         name = "Matthias Wippich";
         email = "mfwippich@gmail.com";
